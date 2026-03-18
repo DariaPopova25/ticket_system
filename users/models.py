07459d5b33ec7ed86ba2e0c9375799663bd214c8
DariaPopova25 <meuw.meuw20@gmail.com>
2026-03-18 17:18:41 +0400

    feat: create a custom User model and apply migrations

---
+++ users/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        CLIENT = "client", "Client"
+        MANAGER = "manager", "Manager"
+        DEVELOPER = "developer", "Developer"
+
+    email = models.EmailField(unique=True)
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role,
+        default=Role.CLIENT,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
