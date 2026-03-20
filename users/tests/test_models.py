7b9efe0ec20c580bdb14c192456a975caaabba32
DariaPopova25 <meuw.meuw20@gmail.com>
2026-03-20 12:17:49 +0400

    test(users): add model tests for User

---
+++ users/tests/test_models.py
@@ -0,0 +1,63 @@
+import pytest
+from django.core.exceptions import ValidationError
+from users.models import User
+from .factories import UserFactory
+
+@pytest.mark.django_db
+def test_creates_user_with_default_role():
+    user_name = 'user_example'
+    user_email = 'user@example.com'
+
+    user = UserFactory.create(
+        username = user_name,
+        email = user_email,
+    )
+
+    assert user.username ==  user_name
+    assert user.email == user_email
+    assert user.role == User.Role.CLIENT
+
+
+@pytest.mark.django_db
+class TestUserEmail:
+    def test_accepts_valid_email(self):
+        user = UserFactory.build(email="test@example.com")
+
+        user.full_clean()
+
+    def test_rejects_duplicate_email(self):
+        email = "duplicate@example.com"
+
+        UserFactory.create(email=email)
+
+        duplicate_user = UserFactory.build(email=email)
+
+        with pytest.raises(ValidationError) as excinfo:
+            duplicate_user.full_clean()
+        
+        assert 'email' in excinfo.value.message_dict
+
+@pytest.mark.django_db
+class TestUserRole:
+    @pytest.mark.parametrize(
+            "role",
+            [
+                User.Role.CLIENT,
+                User.Role.MANAGER,
+                User.Role.DEVELOPER,
+            ]
+    )
+    def test_accepts_allowed_roles(self, role):
+        user = UserFactory.build(role=role)
+
+        user.full_clean()
+
+        assert user.role == role
+
+    def test_rejects_invalid_role(self):
+        user = UserFactory.build(role="worker")
+
+        with pytest.raises(ValidationError) as excinfo:
+            user.full_clean()
+        
+        assert 'role' in excinfo.value.message_dict
