b3a58ca4d9e8a3575fec6057959f2bf7c0c8607f
DariaPopova25 <meuw.meuw20@gmail.com>
2026-03-30 10:23:14 +0400

    test(factories): remove unused fields from comment factory

--- tickets/tests/factories.py
+++ tickets/tests/factories.py
@@ -13,11 +13,8 @@ class TicketFactory(factory.django.DjangoModelFactory):
     description = factory.Faker("text", max_nb_chars=200)
 
 
-# TODO: improve CommentFactory when role-based comment restrictions are added.
 class CommentFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Comment
 
-    ticket = factory.SubFactory(TicketFactory)
-    user = factory.SubFactory(ClientFactory)
     body = factory.Faker("text", max_nb_chars=200)
