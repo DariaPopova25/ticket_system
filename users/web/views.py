42709ef645c119cef442bc7557c02d8d35ecf4c4
DariaPopova25 <meuw.meuw20@gmail.com>
2026-04-03 16:14:58 +0400

    feat(users): add registration and login views

---
+++ users/web/views.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect
+from django.contrib.auth import login
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from users.forms import UserRegistrationForm
+
+
+class UserRegisterView(CreateView):
+    form_class = UserRegistrationForm
+    template_name = "users/register.html"
+
+    def form_valid(self, form):
+        self.object = form.save()
+        login(self.request, self.object)
+        return redirect("tickets:list")
+
+class UserLoginView(LoginView):
+    template_name = "registration/login.html"
+    redirect_authenticated_user = True
+    next_page = reverse_lazy("tickets:list")
